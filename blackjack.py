325478f23670b18c40356a77d21d7123158bb9ae
napoleon lazo <napoleon.lazo@hotmail.com>
2018-07-30 12:03:22 -0500

    Player can make a bet

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,33 @@
 from card import create_deck
 from dealer import Dealer
 from player import Player
+import colorama
+colorama.init()
+
+def call_bets(player):
+
+	while True:
+		try:
+			print(colorama.Fore.GREEN, f"Current amount of chips: {player.chips}")
+			print(colorama.Style.RESET_ALL)
+			quantity = int(input("How much do you want to bet?\n"))
+		except:
+			print(colorama.Fore.RED, "That is not a number, please enter a number")
+			print(colorama.Style.RESET_ALL)
+		else:
+			bet = player.bet(quantity)
+			if(bet):
+				break
+	return bet
 
 if __name__ == '__main__':
 	dealer = Dealer(create_deck())
 	player = Player()
 	playing = True
+	pot = 0
 
-	
+	while playing:
+		dealer.shuffle_deck()
+		pot += call_bets(player)
+		print(f"Current pot: {pot}")
+		break
--- player.py
+++ player.py
@@ -1,3 +1,6 @@
+import colorama 
+
+
 class Player():
 	"""docstring for Player"""
 
@@ -5,8 +8,12 @@ class Player():
 	chips = 500
 
 	def bet(self, quantity):
-		if(self.chips - quantity > 0):
-			return bet
-		else:
-			print("You don't have enough chips")
-		
+		while True:
+			if(self.chips - quantity > 0):
+				self.chips = self.chips - quantity
+				bet = quantity
+				return bet
+			else:
+				print(colorama.Fore.RED, "You don't have enough chips")
+				print(colorama.Style.RESET_ALL)
+				return
