472e28d2df8593183ca57e2c8a6c10e24cac344f
napoleon lazo <napoleon.lazo@hotmail.com>
2018-08-02 14:23:26 -0500

    Refactored player's set_bet function

--- blackjack.py
+++ blackjack.py
@@ -1,24 +1,6 @@
 from card import create_deck
 from dealer import Dealer
-from player import Player
-import colorama
-colorama.init()
-
-def call_bets(player):
-
-	while True:
-		try:
-			print(colorama.Fore.GREEN, f"Current amount of chips: {player.chips}")
-			print(colorama.Style.RESET_ALL)
-			quantity = int(input("How much do you want to bet?\n"))
-		except:
-			print(colorama.Fore.RED, "That is not a number, please enter a number")
-			print(colorama.Style.RESET_ALL)
-		else:
-			bet = player.bet(quantity)
-			if(bet):
-				break
-	return bet
+from player import Player	
 
 if __name__ == '__main__':
 	dealer = Dealer(create_deck())
@@ -28,6 +10,6 @@ if __name__ == '__main__':
 
 	while playing:
 		dealer.shuffle_deck()
-		pot += call_bets(player)
+		pot += player.set_bet()
 		print(f"Current pot: {pot}")
 		break
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import colorama 
+colorama.init()
 
 
 class Player():
@@ -7,7 +8,23 @@ class Player():
 	hand = []
 	chips = 500
 
-	def bet(self, quantity):
+	def set_bet(self):
+
+		while True:
+			try:
+				print(colorama.Fore.GREEN, f"Current amount of chips: {self.chips}")
+				print(colorama.Style.RESET_ALL)
+				quantity = int(input("How much do you want to bet?\n"))
+			except:
+				print(colorama.Fore.RED, "That is not a number, please enter a number")
+				print(colorama.Style.RESET_ALL)
+			else:
+				bet = self.check_chips_availability(quantity)
+				if(bet):
+					break
+		return quantity
+
+	def check_chips_availability(self, quantity):
 		while True:
 			if(self.chips - quantity > 0):
 				self.chips = self.chips - quantity
